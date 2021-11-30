d17d59204320c9aba221c1e996fafe4b06a5b8d9
Alessandro Giappichini <alessandrogiappichini@hotmail.com>
2021-11-30 10:30:14 +0100

    Situazione iniziale

---
+++ app/src/main/java/itts/voltapg/gruppo5c/MainActivity.java
@@ -0,0 +1,14 @@
+package itts.voltapg.gruppo5c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
